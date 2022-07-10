cd72992affd25790f8b8b6f0fbe102f1ece1ac8f
josdoaitran <josdoaitran@gmail.com>
2022-07-10 10:03:23 +0700

    update maven example

--- maven-automation-test-feedback-app-selenium-java-basic/src/TestFeedbackApp.java
+++ maven-automation-test-feedback-app-selenium-java-basic/src/TestFeedbackApp.java
@@ -12,80 +12,80 @@ public class TestFeedbackApp {
     public static void main(String[] args) throws ClassNotFoundException {
         resetData();
         System.setProperty("webdriver.chrome.driver", "D:\\coding\\libs\\chromedriver.exe");
-        WebDriver intChromeDriver = new ChromeDriver();
-        intChromeDriver.get("http://127.0.0.1:5000");
-        intChromeDriver.manage().window().maximize();
-        intChromeDriver.manage().window().minimize();
-        intChromeDriver.manage().window().fullscreen();
-        intChromeDriver.manage().window().maximize();
-        WebElement testing4EveryoneIcon = intChromeDriver.findElement(By.className("logo"));
-        Boolean checkFeedbackPageDisplayed = testing4EveryoneIcon.isDisplayed();
-        if (checkFeedbackPageDisplayed){
-            System.out.println("We are in Feedback App");
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("http://127.0.0.1:5000/");
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().window().minimize();
+        chromeDriver.manage().window().fullscreen();
+        chromeDriver.manage().window().maximize();
+        Boolean isCheckLogoElement = chromeDriver.findElement(By.className("logo")).isDisplayed();
+        if (isCheckLogoElement){
+            System.out.println("We are in Feedback app.");
         }
-        intChromeDriver.findElement(By.name("customer")).sendKeys("Testing4Everyone");
-        Select consultDropdown = new Select(intChromeDriver.findElement(By.name("consultant")));
-        consultDropdown.selectByIndex(1);
-        consultDropdown.selectByValue("Mayra Riggs");
-        consultDropdown.selectByVisibleText("Ali Petty");
-        List<WebElement> listRating = intChromeDriver.findElements(By.xpath("//input[@name='rating']"));
-//        System.out.println(String.valueOf(generateRandomValue(0,listRating.size())));
-        listRating.get(generateRandomValue(0,listRating.size())).click();
-        intChromeDriver.findElement(By.name("comments")).sendKeys("Here is my comments by automation test script");
-        intChromeDriver.findElement(By.className("btn")).click();
-        String message = intChromeDriver.findElement(By.xpath("//h2")).getText();
+        chromeDriver.findElement(By.name("customer")).sendKeys("Test4");
+        Select consultantSelect = new Select(chromeDriver.findElement(By.name("consultant")));
+        consultantSelect.selectByIndex(3);
+        consultantSelect.selectByValue("Mayra Riggs");
+        List<WebElement> ratingRadio = chromeDriver.findElements(By.xpath("//input[@name='rating']"));
+        System.out.println(ratingRadio.size());
+        ratingRadio.get(8).click();
+        ratingRadio.get(generateRandomIndex(0, ratingRadio.size())).click();
+        chromeDriver.findElement(By.name("comments")).sendKeys("I like what you shared");
+        chromeDriver.findElement(By.className("btn")).click();
 
-        if (message == ("Thank you for rating your experience. We hope to see you soon!")){
-            System.out.println("We input the feedback successfully");
+        String textMessage = chromeDriver.findElement(By.xpath("//h2")).getText();
+        if (textMessage.equalsIgnoreCase("Thank you for rating your experience. We hope to see you soon!")){
+            System.out.println("You're already done");
         }
-        ResultSet resultSet = checkDatabase();
-        try {
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("customer"));
-                System.out.println(resultSet.getString("consultant"));
+
+        ResultSet result = checkDatabase();
+        try{
+            while (result.next()){
+                System.out.println(result.getString("customer"));
+                System.out.println(result.getString("consultant"));
             }
         }catch (Exception e){
-
+            System.out.println("Can not retrieve to result data");
         }
-        intChromeDriver.close();
+
     }
 
-    private static int generateRandomValue(int min, int max){
+    private static int generateRandomIndex(int min, int max){
         Random newRandom = new Random();
-        return newRandom.nextInt(max - min) + min ;
+        return newRandom.nextInt(max - min) + min;
     }
 
     private static void resetData() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
         final String url = "jdbc:postgresql://192.168.1.101:5432/feedbackdb";
-        final String username = "postgres";
-        final String password="Password@12345";
-        String sqlStatement = "Delete from public.feedback where customer = 'Testing4Everyone';";
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        final String userName = "postgres";
+        final String password = "Password@12345";
+
+        String sqlStatement = "Delete from feedback where customer = 'Test4'";
+        try(Connection connection = DriverManager.getConnection(url, userName, password))
+        {
             Statement statement = connection.createStatement();
-            boolean resultSet = statement.execute(sqlStatement);
+            statement.execute(sqlStatement);
             connection.close();
-        } catch (SQLException e) {
-            System.out.println("Connection failure.");
-            e.printStackTrace();
+        }catch (SQLException e){
+            System.out.println("Connection to db is failed");
         }
     }
 
     private static ResultSet checkDatabase() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
         final String url = "jdbc:postgresql://192.168.1.101:5432/feedbackdb";
-        final String username = "postgres";
-        final String password="Password@12345";
-        String sqlStatement = "select customer, consultant from public.feedback where customer = 'Testing4Everyone';";
-        ResultSet resultSet = null;
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            Statement statement = connection.createStatement();
-            resultSet = statement.executeQuery(sqlStatement);
-            return resultSet;
-        } catch (SQLException e) {
-            System.out.println("Connection failure.");
-            e.printStackTrace();
+        final String userName = "postgres";
+        final String password = "Password@12345";
+        String sqlStatement = "select customer, consultant from public.feedback where customer = 'Test4'";
+        ResultSet result = null;
+        try(Connection connection = DriverManager.getConnection(url, userName, password)){
+            Statement st = connection.createStatement();
+            result = st.executeQuery(sqlStatement);
+            return  result;
+        }catch (SQLException e){
+            System.out.println("Connection to db is failed");
         }
-        return resultSet;
+        return result;
     }
 }
